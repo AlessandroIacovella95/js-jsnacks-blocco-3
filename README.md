fc17b99b9e2016bdb7eccfcbd1839f5ebe853938
AlessandroIacovella95 <alessandroiacovella95@gmail.com>
2023-07-26 16:12:13 +0200

    js: snack-2

--- README.md
+++ README.md
@@ -11,3 +11,16 @@ Chiedi all'utente un primo numero, poi un secondo e genera un numero casuale com
 - Chiedo all'utente un secondo numero
 - genero un numero casuale compreso tra i due numeri
 
+<hr>
+
+## Traccia Snack-2:
+Dare la possibilità di inserire due parole. Verificare che le due parole abbiano la stessa lunghezza. Se hanno la stessa lunghezza, stamparle entrambe altrimenti stampare la più lunga delle due.
+
+## Svolgimento:
+- Chiedo all'utente una prima parola
+- Chiedo all'utente una seconda parola
+- **SE** la lunghezza delle parole è uguale
+  - Stampo entrambe le parole
+- **ALTRIMENTI SE** la prima parola è più lunga della seconda stampo la prima
+- **ALTRIMENTI** Stampo la seconda
+
--- js/script.js
+++ js/script.js
@@ -1,13 +1,40 @@
+// Esercizio 1
+
 //* Chiedo all'utente un primo numero
 
-const firstNumber = parseInt(prompt('Inserisci un primo numero'))
-console.log(firstNumber);
+// const firstNumber = parseInt(prompt('Inserisci un primo numero'))
+// console.log(firstNumber);
 
-//* Chiedo all'utente un secondo numero
-const secondNumber = parseInt(prompt('Inserisci un secondo numero'))
-console.log(secondNumber);
+// //* Chiedo all'utente un secondo numero
+// const secondNumber = parseInt(prompt('Inserisci un secondo numero'))
+// console.log(secondNumber);
 
-//* Genero un numero casuale compreso tra i numeri scelti dall'utente
-let randomNumber = Math.floor(Math.random () * (secondNumber - firstNumber)) + firstNumber
+// //* Genero un numero casuale compreso tra i numeri scelti dall'utente
+// let randomNumber = Math.floor(Math.random () * (secondNumber - firstNumber)) + firstNumber
 
-console.log('numero compreso: ' + randomNumber);
+// console.log('numero compreso: ' + randomNumber);
+
+
+// Esercizio 2
+
+//* Chiedo all'utente una prima parola
+const firstWord = prompt('Inserisci una prima parola')
+
+//* Chiedo all'utente una seconda parola
+const secondWord = prompt('Inserisci una seconda parola')
+
+//* Chiedo all'utente una prima parola
+if (firstWord.length == secondWord.length) {
+    //* Stampo entrambe le parole
+    console.log(firstWord, secondWord)
+
+    //* ALTRIMENTI SE la prima parola è più lunga della seconda
+} else if  (firstWord.length > secondWord.length){
+
+    //* Stampo la prima
+    console.log(firstWord)
+
+    //* ALTRIMENTI Stampo la seconda
+} else{
+    console.log(secondWord)
+}
